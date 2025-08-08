9cac47e94ce4e3ed8fda6911b32da8786970ea4d
Maverick-Run <jake.postlewaight@gmail.com>
2025-08-09 11:09:33 +1200

    Meme Leaderboard info

---
+++ src/services/MemeLeaderboard.ts
@@ -0,0 +1,21 @@
+/**
+ * In here needs to go the code for getting/setting the top memer of the week
+ * Score conditions:
+ * - Sum of unique reactions
+ * - Total count of reactions on a single post
+ * 
+ * The final score is whichever is less
+ * IE:
+ * If a user has 100 unique reactions and 50 total reactions on a single post,
+ * the score would be 50.
+ * 
+ * In the case of a tie, do whatever you think is best.
+ * 
+ * The user with the Highest score should
+ * - Get announced in the meme channel
+ * - Have the "Top Memer of the Week" role assigned to them
+ * - Have their name on the channel as '<NAME>'s - memes'
+ * 
+ * 
+ * This should occur Monday every week
+ */
